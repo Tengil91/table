cb626c8bfd66ab02fdf0d441e5a54f6e1c0bba2a
niklas <niklas.silfverhielm@triggerbee.com>
2020-03-12 15:58:59 +0100

    'first'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,86 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'table';
+  
+  columnTitles = [
+    "Title",
+    "Url",
+    "Prerequisite Url",
+    "Fullfillments"
+  ];
+  columnDataNames = [
+    "title",
+    "url",
+    "prerequisticUrl",
+    "fullfillments"
+  ]
+  rows = [
+    {
+      title: "My first awsome goal",
+      url: "/contact-us",
+      prerequisticUrl: null,
+      fullfillments: 320
+    },
+    {
+      title: "Sombody elso",
+      url: "/product-1",
+      prerequisticUrl: "/about-product-1",
+      fullfillments: 160
+    },
+    {
+      title: "Important goal",
+      url: null,
+      prerequisticUrl: null,
+      fullfillments: 123
+    },
+    {
+      title: "Important goal",
+      url: null,
+      prerequisticUrl: null,
+      fullfillments: 123
+    },
+    {
+      title: "Important goal",
+      url: null,
+      prerequisticUrl: null,
+      fullfillments: 123
+    },
+    {
+      title: "Important goal",
+      url: null,
+      prerequisticUrl: null,
+      fullfillments: 123
+    },
+    {
+      title: "Important goal",
+      url: null,
+      prerequisticUrl: null,
+      fullfillments: 123
+    },
+    {
+      title: "Important goal",
+      url: null,
+      prerequisticUrl: null,
+      fullfillments: 123
+    },
+    {
+      title: "Important goal",
+      url: null,
+      prerequisticUrl: null,
+      fullfillments: 123
+    },
+    {
+      title: "Important goal",
+      url: null,
+      prerequisticUrl: null,
+      fullfillments: 123
+    },
+    {
+      title: "Important goal",
+      url: null,
+      prerequisticUrl: null,
+      fullfillments: 123
+    },
+  ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatMenuModule } from '@angular/material/menu';
+
+import { MatIconModule } from '@angular/material/';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
